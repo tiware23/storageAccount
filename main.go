c99a30078abc18c733167b98a309234053106e3d
thiago.nascimento <tiware23@gmail.com>
2020-06-29 11:14:15 -0300

    Update main.go

--- main.go
+++ main.go
@@ -4,21 +4,56 @@ import (
 	"fmt"
 	"os"
 
+	"flag"
+
 	f "github.com/tiware23/storageAccount/syncfile"
 )
 
+func setAccountStorage(fileStorage *f.AssetFile) {
+
+	fileStorage.SetAccountVars()
+	fileStorage.GetCrendials()
+	fileStorage.ParseContainerURL()
+
+}
+
+func uploadFile(fileUpload f.AssetFile) {
+	fmt.Println("Uploading files...")
+	fmt.Println("")
+
+	fileName := flag.Args()
+	for _, f := range fileName {
+		fmt.Println("Uploading the file: ", f, "to Blob: ", fileUpload.ContainerName)
+		fmt.Println(f)
+		fileUpload.UploadToBlob(f)
+	}
+}
+
 func main() {
 
-	jsonFile := f.AssetFile{ContentType: "application/json", ContainerName: "static",
-		AccountName: "accountName", AccountKey: "accountKey"}
+	commFile := flag.Int("ftype", 1, "Choose Option")
+	accName := flag.String("stacc", "cards", "Storage Account Name")
+	contType := flag.String("ctype", "application/json", "File's content type")
+	contName := flag.String("ctame", "static", "Container Name")
 
-	jsonFile.SetAccountVars()
-	jsonFile.GetCrendials()
-	jsonFile.ParseContainerURL()
+	flag.Parse()
 
-	fileName := os.Args[1:]
-	for _, f := range fileName {
-		fmt.Println("Uploading the file: ", f)
-		jsonFile.UploadToBlob(f)
+	switch *commFile {
+	case 1:
+		jsonFile := f.AssetFile{ContentType: *contType, ContainerName: *contName, AccountName: *accName}
+		setAccountStorage(&jsonFile)
+		uploadFile(jsonFile)
+		os.Exit(0)
+
+	case 2:
+		textFile := f.AssetFile{ContentType: *contType, ContainerName: *contName, AccountName: *accName}
+		setAccountStorage(&textFile)
+		uploadFile(textFile)
+		os.Exit(0)
+
+	default:
+		fmt.Println("No option available")
+		os.Exit(-1)
+
 	}
 }
